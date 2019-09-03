b6240de97bdcca669de8b25c7a382227759d13c6
yuehua <wuyuehua5@gmail.com>
2019-09-03 10:57:13 +0800

    Add test_dio

--- test/test_dio.dart
+++ test/test_dio.dart
@@ -5,7 +5,6 @@ import 'dart:io';
 // 如果在公司有代理去访问，有问题！
 // DioError [DioErrorType.DEFAULT]: SocketException: OS Error: Operation timed out, errno = 60, address = test001.btylx.com, port = 56668
 void main() async {
-
   String baseUrl = 'https://glacier.btylx.com:6687';
   String pd = '';
   String token = '';
@@ -19,17 +18,29 @@ void main() async {
   // add cookie manager
   var cj = CookieJar();
   glacierDio.interceptors.add(CookieManager(cj));
-  glacierDio.interceptors.add(LogInterceptor(responseBody: false));
+  //glacierDio.interceptors.add(LogInterceptor(responseBody: false));
 
-
+  // First accsee URL, get pd, token, Tt from server
   try {
-    Response response = await glacierDio.get('/tcgindex?a=published')
-        .whenComplete(() {
-          print('First dio request finish');
-        });
-
-    var cookies = cj.loadForRequest(
-        Uri.parse(baseUrl + '/tcgindex?a=published'));
+    Response response = await glacierDio.get('/tcgindex?a=published');
+    /*
+    print(response.headers['set-cookie']);
+    print(response.headers['set-cookie'].runtimeType);
+    if (response.headers['set-cookie'] != null) {
+      for (var cookieStr in response.headers['set-cookie']) {
+        // if cookie name is pd, it should be like this 'pd=4029fa87fda17467; Path=/; HttpOnly'
+        if (cookieStr.startsWith('pd=')) {
+          // 'pd=' is first 3 chars, so startIndex is 3 to get the value.
+          int startIndex = 3;
+          int endIndex = cookieStr.indexOf(';');
+          String currentPD = cookieStr.substring(startIndex, endIndex);
+          print('current pd is:' + currentPD);
+        }
+      }
+    }
+*/
+    var cookies =
+        cj.loadForRequest(Uri.parse(baseUrl + '/tcgindex?a=published'));
     //cookies.forEach((element) => print(element));
     for (var cookie in cookies) {
       //print(cookie.name +":" + cookie.value);
@@ -58,76 +69,101 @@ void main() async {
     }
   }
 
-  print('pd: ' + pd + ', ' + 'token: ' + token + ', ' + 'tt: ' + tt +'\n');
+  print('pd: ' + pd + ', ' + 'token: ' + token + ', ' + 'tt: ' + tt + '\n');
 
+  /*
+  var newCookies = [
+    Cookie('pd', pd),
+    Cookie('token', token),
+    Cookie('Tt', tt)
+  ];
+*/
 
   var dio = Dio(BaseOptions(
     baseUrl: baseUrl,
     connectTimeout: 5000, // 5s
   ));
-  // construct cookie and send request
-  List<Cookie> newCookies = [
-    Cookie('pd', pd),
-    Cookie('token', token),
-    Cookie('Tt', tt)
-  ];
-  //dio.options.cookies = newCookies;
-  final httpHeaders= {
-    'Accept': 'application/json, text/plain, */*',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'zh-CN,zh;q=0.9',
-    'Connection': 'keep-alive',
-    'Content-Type': 'application/json',
-    'Cookie': 'pd=$pd; token=$token; Tt=$tt',
-  };
-  var newCJ = CookieJar();
-  //newCJ.saveFromResponse((Uri.parse(baseUrl + '/tcgindex?a=published')), newCookies);
 
-  //dio.interceptors.add(CookieManager(newCJ));
-  dio.options.headers = httpHeaders;
-  //dio.options.method = 'POST';
-  dio.interceptors.add(InterceptorsWrapper(
-    onResponse: (Response response) {
-      print(response.statusCode);
-      print(response.headers);
-      return response;
+
+  dio.interceptors.add(InterceptorsWrapper(onRequest: (Options options) async {
+    print('-----begin check current pd in server -----');
+    dio.interceptors.requestLock.lock();
+    // use a new Dio(to avoid dead lock) instance to check server pd.
+    var tempDio = Dio(BaseOptions(
+      baseUrl: baseUrl,
+      connectTimeout: 5000,
+    ));
+    var tempCookies = [
+      Cookie('pd', pd),
+      Cookie('token', token),
+      Cookie('Tt', tt)
+    ];
+    var tempCJ = CookieJar();
+    tempCJ.saveFromResponse(Uri.parse(baseUrl + '/tcgindex?a=published'), tempCookies);
+    tempDio.interceptors.add(CookieManager(tempCJ));
+    tempDio.interceptors.add(LogInterceptor(responseBody: false));
+    Response res = await tempDio.get("/tcgindex?a=published");
+
+
+    if (res.headers['set-cookie'] != null) {
+      for (var cookieStr in res.headers['set-cookie']) {
+        // if cookie name is pd, it should be like this 'pd=4029fa87fda17467; Path=/; HttpOnly'
+        if (cookieStr.startsWith('pd=')) {
+          // 'pd=' is first 3 chars, so startIndex is 3 to get the value.
+          int startIndex = 3;
+          int endIndex = cookieStr.indexOf(';');
+          String currentPD = cookieStr.substring(startIndex, endIndex);
+          print('current pd is:' + currentPD);
+          // use current pd replace old pd
+
+
+
+          pd = currentPD;
+        }
+      }
     }
-  ));
+
+    var newCookies = [
+      Cookie('pd', pd),
+      Cookie('token', token),
+      Cookie('Tt', tt)
+    ];
+    cj.saveFromResponse(Uri.parse(baseUrl + '/tcgindex?a=published'), newCookies);
+    dio.interceptors.requestLock.unlock();
+    print('-----complete check current pd in server -----');
+    print('\n');
+    print('\n');
+    return options; //continue
+  }));
+  //cj.saveFromResponse(Uri.parse(baseUrl + '/tcgindex?a=published'), newCookies);
+  dio.interceptors.add(CookieManager(cj));
   dio.interceptors.add(LogInterceptor(responseBody: false));
+
+
+/*
+  var newCookies = [
+  Cookie('pd', p),
+  Cookie('token', token),
+  Cookie('Tt', tt)
+  ];
+  cj.saveFromResponse(Uri.parse(baseUrl + '/tcgindex?a=published'), newCookies);
+  dio.interceptors.add(CookieManager(cj));
+  dio.interceptors.add(LogInterceptor(responseBody: false));
+*/
   try {
 
-    //print('Begin to sleep 100s');
-   //await sleep(Duration(seconds: 100));
-   // print('Sleep finish');
+    var res = await dio.get('/scenic?a=defs&name=%E6%99%AF%E5%B1%B1%E5%85%AC%E5%9B%AD');
+    // print(newCJ.loadForRequest((Uri.parse(baseUrl + '/tcgindex?a=published'))));
 
-    var res = Response();
-    res = await dio.post('/tcgindex?a=published');
-   // print(newCJ.loadForRequest((Uri.parse(baseUrl + '/tcgindex?a=published'))));
-    /*
-    print('begin to sleep...');
-    await sleep(Duration(seconds: 60));
-    print('sleep complete...');
-    */
-    //print('1:');
-   // print(dio.options.headers.toString());
-    //print(res.headers.toString());
+    print('begin to sleep 30s');
+    await sleep(Duration(seconds: 30));
+    print('sleep 30s complete...');
+    res = await dio.get('/scenic?a=defs&name=%E6%99%AF%E5%B1%B1%E5%85%AC%E5%9B%AD');
 
-    //await sleep(Duration(seconds: 100));
-    //res = await dio.get('/tcgindex?a=published');
-    //print('2' );
-    //print(dio.options.headers.toString());
-    //print(res.headers.toString());
-
-
-    //response = await dio.get('/tcgindex?a=published');
-    //print('3' + response.headers.toString());
-
-
-    //response = await dio.get('/tcgindex?a=published');
-    //print('4' + response.headers.toString());
-
-    //response = await dio.get('/tcgindex?a=published');
-    //print('5' + response.headers.toString());
+    print('begin to sleep 100s');
+    await sleep(Duration(seconds: 100));
+    print('sleep 100s complete...');
+    res = await dio.get('/scenic?a=defs&name=%E6%99%AF%E5%B1%B1%E5%85%AC%E5%9B%AD');
 
   } on DioError catch (e) {
     if (e.response != null) {
@@ -141,6 +177,3 @@ void main() async {
     }
   }
 }
-
-
-
